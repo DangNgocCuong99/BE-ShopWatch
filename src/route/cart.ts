71de98bba6aa3d91f11e1373d7957b89393254ff
NamNguyen27 <namkudo2001@gmail.com>
2024-01-10 22:58:36 +0700

    dung xoa cart

--- src/route/cart.ts
+++ src/route/cart.ts
@@ -1,10 +1,11 @@
 import express from "express"
-import {  getCart,addCart} from "../controller/cart"
+import {  getCart,addCart, removeCart} from "../controller/cart"
 import { protect } from "../controller/auth"
 const routerCart = express.Router()
 routerCart.use(protect)
 
 routerCart.get('/', getCart )
 routerCart.post('/', addCart )
+routerCart.delete('/:id',removeCart)
 
 export default routerCart
