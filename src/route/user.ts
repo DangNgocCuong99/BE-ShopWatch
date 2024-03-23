448a219c9f85f69ae2310aef1b7bd2c7ca7d5d01
NamNguyen27 <namkudo2001@gmail.com>
2024-03-23 18:11:19 +0700

    fix path

--- src/route/user.ts
+++ src/route/user.ts
@@ -4,7 +4,7 @@ import { protect, restrictTo } from "../controller/auth"
 const routerUser = express.Router()
 routerUser.use(protect)
 routerUser.get("/current",getCurrentUser)
-routerUser.patch('/current/:id', updateCurrentUser )
+routerUser.put('/current', updateCurrentUser )
 routerUser.get('/',restrictTo("admin"), getUser )
 routerUser.get('/:id', detailUser )
 routerUser.patch('/:id', changeActiveUser )
